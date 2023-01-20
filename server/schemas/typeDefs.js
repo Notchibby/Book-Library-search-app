d9b2c1960ed9ca1e9d573205b65fa09dec2a3ef2
Chibuike Okoye~ <talk2chibyke@yahoo.com>
2023-01-20 11:31:44 +1100

    typedefs queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,42 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-`;
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks; [Book]
+  }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-// export the typeDefs
+`
 module.exports = typeDefs;
